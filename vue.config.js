af8e703c8bdf95c217fd86bf7c7781d7d483af5e
Vladislav0101 <by.vlad01@mail.ru>
2021-03-04 15:12:38 +0300

    vue config update

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  productionSourceMap: false
+  productionSourceMap: false,
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/Innowise-Lab-Internship-Level-1-Clever-to-do-list/"
+      : "/"
 };
